6c66c7721e0a9106e7c977b87f1bae25d56c9984
Francisco_Diaz_Roldan <pacodiaz008@gmail.com>
2023-10-13 20:30:12 +0200

    .

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,12 @@
 package org.example;
 
-import static org.example.ProcesadorArchivos.procesadorArchicos;
+import static org.example.ProcesadorArchivos.procesadorArchivos;
 
 public class Main {
     public static void main(String[] args) {
-        procesadorArchicos("data.csv", "template.txt");
+
+        //Se llama al método procesadorArchivos con los nombres de los archivos necesarios para ejecutarlo
+        procesadorArchivos("data.csv","template.txt");
     }
+
 }
--- src/main/java/org/example/ProcesadorArchivos.java
+++ src/main/java/org/example/ProcesadorArchivos.java
@@ -5,16 +5,18 @@ import java.util.ArrayList;
 
 public class ProcesadorArchivos {
 
-    public static void procesadorArchicos(String archivoCsv, String archivoPlantilla) {
+    public static void procesadorArchivos(String archivoCsv, String archivoPlantilla) {
         File archivo = new File(archivoCsv);
+
+        boolean carpetaCreada = false;
 
         try (BufferedReader brArchivo = new BufferedReader(new FileReader(archivo))) {
             String lecturaArchivo;
-            int numeroLinea = 0; // Variable para el número de línea
+            int linea = 0; // Variable para el número de línea
             while ((lecturaArchivo = brArchivo.readLine()) != null) {
-                numeroLinea++; // Incrementa el número de la línea
+                linea++; // Incrementa el número de la línea
 
-                boolean datosCompletos = true; // Variable para controlar si todos los datos están completos
+                boolean todosDatos = true; // Variable para controlar si todos los datos están completos
                 String[] datosArchivo = lecturaArchivo.split(",");
                 ArrayList<String> elementosFaltantes = new ArrayList<>();
 
@@ -31,55 +33,71 @@ public class ProcesadorArchivos {
                     if (email.isEmpty()) elementosFaltantes.add("email");
                     if (empleado.isEmpty()) elementosFaltantes.add("empleado");
 
-                    if (!elementosFaltantes.isEmpty()) {
-                        datosCompletos = false;
-                    }
+                    if (elementosFaltantes.isEmpty()) {
+                        ArrayList<String> plantillas = new ArrayList<>();
+                        try (BufferedReader brPlantilla = new BufferedReader(new FileReader(archivoPlantilla))) {
+                            String lecturaPlantilla;
+                            while ((lecturaPlantilla = brPlantilla.readLine()) != null) {
+                                lecturaPlantilla = lecturaPlantilla.replace("%%2%%", ciudad);
+                                lecturaPlantilla = lecturaPlantilla.replace("%%3%%", email);
+                                lecturaPlantilla = lecturaPlantilla.replace("%%4%%", empresa);
+                                lecturaPlantilla = lecturaPlantilla.replace("%%5%%", empleado);
 
-                    ArrayList<String> plantillas = new ArrayList<>();
-                    try (BufferedReader brPlantilla = new BufferedReader(new FileReader(archivoPlantilla))) {
-                        String lecturaPlantilla;
-                        while ((lecturaPlantilla = brPlantilla.readLine()) != null) {
-                            lecturaPlantilla = lecturaPlantilla.replace("%%2%%", ciudad);
-                            lecturaPlantilla = lecturaPlantilla.replace("%%3%%", email);
-                            lecturaPlantilla = lecturaPlantilla.replace("%%4%%", empresa);
-                            lecturaPlantilla = lecturaPlantilla.replace("%%5%%", empleado);
-
-                            plantillas.add(lecturaPlantilla + "\n");
-                            System.out.println(lecturaPlantilla);
-
-                            if (lecturaPlantilla.contains("%%")) {
-                                datosCompletos = false; // Si hay cosas sin reemplazar, establece datosCompletos a false
-                                break; // Sale del bucle si falta algo
-                            }
-                        }
-                    } catch (IOException e) {
-                        throw new RuntimeException("Error al leer el archivo de la plantilla " + e);
-                    }
-
-                    if (datosCompletos) {
-                        File salida = new File("salida");
-                        salida.mkdir();
-
-                        try (BufferedWriter correoBienvenida = new BufferedWriter(new FileWriter("salida/correoBienvenida-" + id + ".txt"))) {
-                            for (String nuevaPlantilla : plantillas) {
-                                correoBienvenida.write(nuevaPlantilla);
-                                correoBienvenida.flush();
+                                plantillas.add(lecturaPlantilla + "\n");
+                                if (lecturaPlantilla.contains("%%")) {
+                                    todosDatos = false; // Si hay cosas sin reemplazar, establece datosCompletos a false
+                                    break; // Sale del bucle si falta algo
+                                }
                             }
                         } catch (IOException e) {
-                            throw new RuntimeException("Error al escribir el archivo de salida " + e);
+                            throw new RuntimeException("Error al leer el archivo de la plantilla " + e);
                         }
-                        System.out.println("\nCreada la carpeta 'salida' y el archivo 'correoBienvenida-" + id + ".txt'");
+
+                        if (todosDatos && !carpetaCreada) {
+                            File salida = new File("salida");
+                            salida.mkdir();
+                            System.out.println("Se ha creado correctamente la carpeta salida:");
+                            carpetaCreada = true; // Marca que la carpeta se ha creado
+                        }
+
+                        if (todosDatos) {
+                            try (BufferedWriter correoBienvenida = new BufferedWriter(new FileWriter("salida/correoBienvenida-" + id + ".txt"))) {
+                                for (String nuevaPlantilla : plantillas) {
+                                    correoBienvenida.write(nuevaPlantilla);
+                                }
+                                System.out.println("Se ha creado correctamente el correoBienvenida-" + id + ".txt");
+                            } catch (IOException e) {
+                                throw new RuntimeException("Error al escribir el archivo de salida " + e);
+                            }
+                        }
                     } else {
-                        String mensajeError = "Error: Faltan datos en la línea " + numeroLinea + ", falta ";
+                        String mensajeError = "Error: Faltan datos en la línea " + linea + ", falta añadir: ";
                         mensajeError += String.join(", ", elementosFaltantes);
-                        System.err.println(mensajeError + " del archivo data.csv");
+                        System.err.println(mensajeError + " en el archivo data.csv");
                     }
                 } else {
-                    System.err.println("Error: Faltan datos en la línea " + numeroLinea + " del archivo data.csv");
+                    System.err.println("Error: Faltan datos en la línea " + linea + " en el archivo data.csv");
                 }
             }
         } catch (IOException e) {
             throw new RuntimeException("Error al leer el archivo de obtención de datos " + e);
+        }
+
+        // Ahora, iteramos a través de los archivos correoBienvenida-*.txt y los imprimimos
+        File salida = new File("salida");
+        if (salida.exists() && salida.isDirectory()) {
+            File[] archivosCorreoBienvenida = salida.listFiles((dir, name) -> name.startsWith("correoBienvenida-") && name.endsWith(".txt"));
+            for (File archivoCorreo : archivosCorreoBienvenida) {
+                //System.out.println("Contenido de " + archivoCorreo.getName() + ":");
+                try (BufferedReader brCorreo = new BufferedReader(new FileReader(archivoCorreo))) {
+                    String linea;
+                    while ((linea = brCorreo.readLine()) != null) {
+                        //System.out.println(linea);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException("Error al leer el archivo de salida " + e);
+                }
+            }
         }
     }
 }
