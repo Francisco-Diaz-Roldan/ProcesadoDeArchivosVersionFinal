7cf6b83069f383924c4cac34510a62f2f08d7cc2
Francisco_Diaz_Roldan <pacodiaz008@gmail.com>
2023-10-13 18:47:04 +0200

    .

--- src/main/java/org/example/ProcesadorArchivos.java
+++ src/main/java/org/example/ProcesadorArchivos.java
@@ -10,18 +10,32 @@ public class ProcesadorArchivos {
 
         try (BufferedReader brArchivo = new BufferedReader(new FileReader(archivo))) {
             String lecturaArchivo;
+            int numeroLinea = 0; // Variable para el número de línea
             while ((lecturaArchivo = brArchivo.readLine()) != null) {
-                boolean datosCompletos = true; // Variable para controlar si todos los datos están completos
+                numeroLinea++; // Incrementa el número de línea
 
+                boolean datosCompletos = true; // Variable para controlar si todos los datos están completos
                 String[] datosArchivo = lecturaArchivo.split(",");
+                ArrayList<String> elementosFaltantes = new ArrayList<>();
+
                 if (datosArchivo.length >= 5) {
-                    ArrayList<String> plantillas = new ArrayList<>();
                     String id = datosArchivo[0];
                     String empresa = datosArchivo[1];
                     String ciudad = datosArchivo[2];
                     String email = datosArchivo[3];
                     String empleado = datosArchivo[4];
 
+                    if (id.isEmpty()) elementosFaltantes.add("id");
+                    if (empresa.isEmpty()) elementosFaltantes.add("empresa");
+                    if (ciudad.isEmpty()) elementosFaltantes.add("ciudad");
+                    if (email.isEmpty()) elementosFaltantes.add("email");
+                    if (empleado.isEmpty()) elementosFaltantes.add("empleado");
+
+                    if (!elementosFaltantes.isEmpty()) {
+                        datosCompletos = false;
+                    }
+
+                    ArrayList<String> plantillas = new ArrayList<>();
                     try (BufferedReader brPlantilla = new BufferedReader(new FileReader(archivoPlantilla))) {
                         String lecturaPlantilla;
                         while ((lecturaPlantilla = brPlantilla.readLine()) != null) {
@@ -34,8 +48,8 @@ public class ProcesadorArchivos {
                             System.out.println(lecturaPlantilla);
 
                             if (lecturaPlantilla.contains("%%")) {
-                                datosCompletos = false; // Si hay marcadores sin reemplazar, establece datosCompletos a false
-                                break; // Sale del bucle si falta algún valor
+                                datosCompletos = false; // Si hay cosas sin reemplazar, establece datosCompletos a false
+                                break; // Sale del bucle si falta algo
                             }
                         }
                     } catch (IOException e) {
@@ -54,13 +68,14 @@ public class ProcesadorArchivos {
                         } catch (IOException e) {
                             throw new RuntimeException("Error al escribir el archivo de salida " + e);
                         }
-
-                        System.out.println("Creada la carpeta 'salida' y el archivo 'correoBienvenida-" + id + ".txt'");
+                        System.out.println("\nCreada la carpeta 'salida' y el archivo 'correoBienvenida-" + id + ".txt'");
                     } else {
-                        System.err.println("Error: Faltan datos en la línea: " + lecturaArchivo + " del archivo data.csv");
+                        String mensajeError = "Error: Faltan datos en la línea " + numeroLinea + ", falta ";
+                        mensajeError += String.join(", ", elementosFaltantes);
+                        System.err.println(mensajeError + " del archivo data.csv");
                     }
                 } else {
-                    System.err.println("Error: Faltan datos en la línea: " + lecturaArchivo + " del archivo data.csv");
+                    System.err.println("Error: Faltan datos en la línea " + numeroLinea + " del archivo data.csv");
                 }
             }
         } catch (IOException e) {
