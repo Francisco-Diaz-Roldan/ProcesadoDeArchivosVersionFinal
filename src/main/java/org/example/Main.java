3a17960af452c1b882fca994f6c6785f4a62fbb8
Francisco_Diaz_Roldan <pacodiaz008@gmail.com>
2023-10-13 20:57:47 +0200

    .

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import static org.example.ProcesadorArchivos.procesadorArchivos;
 
 public class Main {
+
     public static void main(String[] args) {
 
         //Se llama al método procesadorArchivos con los nombres de los archivos necesarios para ejecutarlo
