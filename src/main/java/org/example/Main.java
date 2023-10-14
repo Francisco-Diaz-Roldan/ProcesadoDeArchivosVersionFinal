f7a2e20606d80e45e98fddfdf1d2b66627476999
Francisco_Diaz_Roldan <pacodiaz008@gmail.com>
2023-10-14 13:42:34 +0200

    JavaDoc terminado.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,18 @@
 package org.example;
 
 import static org.example.ProcesadorArchivos.procesadorArchivos;
-
+/**
+ * Esta clase que contiene el método principal "main" que comienza la ejecución del programa.
+ */
 public class Main {
 
-    public static void main(String[] args) {
+    /***
+     * Se trata del método principal que da comienzo al programa de procesado de archivos.
+     *
+     * @param args           Hace referencia a los argumentos de la línea de comandos, no se utilizan en esta ocasión.
+     */
 
+    public static void main(String[] args) {
         //Se llama al método procesadorArchivos con los nombres de los archivos necesarios para ejecutarlo
         procesadorArchivos("data.csv","template.txt");
     }
